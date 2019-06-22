987d527be8b2940cafb705db761a407db1c4b6a0
Fabien <fabien.gadet@epitech.eu>
2019-06-22 16:53:49 +0200

    Move

--- srcs/grid.py
+++ srcs/grid.py
@@ -1,7 +1,17 @@
+from pacu import *
+
 # PACU = P
 # GHOSTU = G
 # CANDYforPACU = C
+# SUPER BONBON = S
+# VOID = V
 # WALLU = W
+
+
+def index_2d(myList, v):
+    for i, x in enumerate(myList):
+        if v in x:
+            return [x.index(v), i]
 
 
 class Grid:
@@ -9,13 +19,19 @@ class Grid:
         self.grid_ = []
         with open(pathToGrid) as g:
             for i in g.readlines():
-                self.grid_.append(i)
+                self.grid_.append(i.split())
+        self.pacu = Pacu(index_2d(self.grid_, "P"))
+
+    def remove_this_trash_pacu(self):
+        pacu_current_pos = index_2d(self.grid_, "P")
+        self.grid_[pacu_current_pos[1]][pacu_current_pos[0]] = 'V'
+
+    def GETTHEFUCKOUTOFMYROOMIMPLAYING_PACU(self, dir):
+        thingsaround = [self.grid_[self.pacu.pos[1] - 1][self.pacu.pos[0]],
+                        self.grid_[self.pacu.pos[1] + 1][self.pacu.pos[0]], self.grid_[self.pacu.pos[1]][self.pacu.pos[0] + 1], self.grid_[self.pacu.pos[1]][self.pacu.pos[0] - 1]]
+        self.pacu.move(dir, thingsaround)
+        self.remove_this_trash_pacu()
+        self.grid_[self.pacu.pos[1]][self.pacu.pos[0]] = 'P'
 
     def __str__(self):
         return ('\n'.join([' '.join([str(cell) for cell in row]) for row in self.grid_]))
-
-
-# Exemple, print la grille
-if __name__ == "__main__":
-    a = Grid()
-    print(a)
--- srcs/main.py
+++ srcs/main.py
@@ -1,6 +1,10 @@
 from grid import *
+import os
 
 if __name__ == "__main__":
     gridu = Grid()
     print("Hi i'm pAIkumanuCH, here is a grid")
-    print(gridu)
+
+    for x in range(3):
+        print(gridu)
+        gridu.GETTHEFUCKOUTOFMYROOMIMPLAYING_PACU("LEFT")
